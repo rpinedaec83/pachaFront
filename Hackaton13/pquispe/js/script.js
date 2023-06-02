93a268b033d23da9392b64333dbf6d8620f923ca
Paulo Quispe <paulo.quispe.horna@gmail.com>
2023-06-02 02:19:31 -0500

    Maquetación del Crud y creacion de estilos con sass (atomic design)

---
+++ Hackaton13/pquispe/js/script.js
@@ -0,0 +1,34 @@
+
+class Dogs {
+    constructor () {
+
+    };
+};
+
+Dogs.prototype.build = function () {
+    return {
+
+    };
+};
+
+class Modals {
+    constructor () {
+
+    };
+};
+
+Modals.prototype.build = function () {
+    return {
+
+    };
+};
+
+const app = function () {
+    return {
+        init: function () {
+            console.log('Estoy funcionando');
+        }
+    };
+}();
+
+app.init();
