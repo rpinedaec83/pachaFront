596b48d8eb28894e3ef61b6a0cebf4bebca2a8e1
Paulo Quispe <paulo.quispe.horna@gmail.com>
2023-06-04 18:13:45 -0500

    Mejoras en el codigo de programacion js y estilos sass

--- Hackaton13/pquispe/js/classModal.js
+++ Hackaton13/pquispe/js/classModal.js
@@ -1,241 +1,246 @@
 
 class Modal {
-    constructor() {
-        this.mensajeModal = '';
-        this.inputNombre = '';
-        this.inputApellido = '';
-        this.inputRaza = '';
-        this.inputTelefono = '';
-        this.inputPais = '';
-        this.inputFotografia = '';
-        this.inputDescripcion = '';
-    };
+  constructor() {
+    this.mensajeModal = '';
+    this.inputNombre = '';
+    this.inputApellido = '';
+    this.inputCorreo = '';
+    this.inputTelefono = '';
+    this.inputPais = '';
+    this.inputFotografia = '';
+    this.inputDescripcion = '';
+  };
 };
 
 Modal.prototype.setMensajeModal = function (addMensajeModal) {
-    this.mensajeModal = addMensajeModal;
-    return this;
+  this.mensajeModal = addMensajeModal;
+  return this;
 };
 
 Modal.prototype.setInputNombre = function (addInputNombre) {
-    this.inputNombre = addInputNombre;
-    return this;
+  this.inputNombre = addInputNombre;
+  return this;
 };
 
 Modal.prototype.setInputApellido = function (addInputApellido) {
-    this.inputApellido = addInputApellido;
-    return this;
+  this.inputApellido = addInputApellido;
+  return this;
 };
 
-Modal.prototype.setInputRaza = function (addInputRaza) {
-    this.inputRaza = addInputRaza;
-    return this;
+Modal.prototype.setInputCorreo = function (addInputCorreo) {
+  this.inputCorreo = addInputCorreo;
+  return this;
 };
 
 Modal.prototype.setInputTelefono = function (addInputTelefono) {
-    this.inputTelefono = addInputTelefono;
-    return this;
+  this.inputTelefono = addInputTelefono;
+  return this;
 };
 
 Modal.prototype.setInputPais = function (addInputPais) {
-    this.inputPais = addInputPais;
-    return this;
+  this.inputPais = addInputPais;
+  return this;
 };
 
 Modal.prototype.setInputFotografia = function (addInputFotografia) {
-    this.inputFotografia = addInputFotografia;
-    return this;
+  this.inputFotografia = addInputFotografia;
+  return this;
 };
 
 Modal.prototype.setInputDescripcion = function (addInputDescripcion) {
-    this.inputDescripcion = addInputDescripcion;
-    return this;
+  this.inputDescripcion = addInputDescripcion;
+  return this;
 };
 
 Modal.prototype.build = function () {
-    return {
-        mensajeModal: this.mensajeModal,
-        inputNombre: this.inputNombre,
-        inputApellido: this.inputApellido,
-        inputRaza: this.inputRaza,
-        inputTelefono: this.inputTelefono,
-        inputPais: this.inputPais,
-        inputFotografia: this.inputFotografia,
-        inputDescripcion: this.inputDescripcion
-    };
+  return {
+    mensajeModal: this.mensajeModal,
+    inputNombre: this.inputNombre,
+    inputApellido: this.inputApellido,
+    inputCorreo: this.inputCorreo,
+    inputTelefono: this.inputTelefono,
+    inputPais: this.inputPais,
+    inputFotografia: this.inputFotografia,
+    inputDescripcion: this.inputDescripcion
+  };
 };
 
 Modal.prototype.cerrarModal = function () {
-    const body = document.getElementById('body');
-    const section = document.querySelector('.formContainer');
+  const body = document.getElementById('body');
+  const section = document.querySelector('.formContainer');
 
-    body.removeChild(section);
+  body.removeChild(section);
 };
 
 Modal.prototype.completarFormulario = function (index) {
-    const body = document.getElementById('body');
-    const section = document.createElement('section');
-    const form = document.createElement('form');
-    const divMensaje = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const img = document.createElement('img');
-    const divInputs = document.createElement('div');
-    const inputNombre = document.createElement('input');
-    const inputApellido = document.createElement('input');
-    const inputRaza = document.createElement('input');
-    const inputTelefono = document.createElement('input');
-    const inputPais = document.createElement('input');
-    const inputFotografia = document.createElement('input');
-    const textarea = document.createElement('textarea');
-    const divButtons = document.createElement('div');
-    const btnAccept = document.createElement('button');
-    const btnCancel = document.createElement('button');
+  const body = document.getElementById('body');
+  const section = document.createElement('section');
+  const form = document.createElement('form');
+  const divMensaje = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const img = document.createElement('img');
+  const divInputs = document.createElement('div');
+  const inputNombre = document.createElement('input');
+  const inputApellido = document.createElement('input');
+  const inputCorreo = document.createElement('input');
+  const inputTelefono = document.createElement('input');
+  const inputPais = document.createElement('input');
+  const inputFotografia = document.createElement('input');
+  const textarea = document.createElement('textarea');
+  const divButtons = document.createElement('div');
+  const btnAccept = document.createElement('button');
+  const btnCancel = document.createElement('button');
 
-    section.setAttribute('class', 'formContainer');
-    form.setAttribute('class', 'form');
-    form.setAttribute('action', '#');
-    form.setAttribute('method', 'post');
-    divMensaje.setAttribute('class', 'form__presentation');
-    h2.setAttribute('class', 'form__title');
-    img.setAttribute('class', 'form__iconClose');
-    img.setAttribute('src', './assets/close.svg');
-    img.setAttribute('alt', 'close');
-    divInputs.setAttribute('class', 'form__inputsContainer');
-    inputNombre.setAttribute('type', 'text');
-    inputNombre.setAttribute('id', 'nombre');
-    inputNombre.setAttribute('name', 'nombre');
-    inputNombre.setAttribute('placeholder', 'Nombre');
-    inputNombre.setAttribute('autocomplete', 'off');
-    inputNombre.setAttribute('value', this.inputNombre);
-    inputApellido.setAttribute('type', 'text');
-    inputApellido.setAttribute('id', 'apellido');
-    inputApellido.setAttribute('name', 'apellido');
-    inputApellido.setAttribute('placeholder', 'Apellido');
-    inputApellido.setAttribute('autocomplete', 'off');
-    inputApellido.setAttribute('value', this.inputApellido);
-    inputRaza.setAttribute('type', 'text');
-    inputRaza.setAttribute('id', 'raza');
-    inputRaza.setAttribute('name', 'raza');
-    inputRaza.setAttribute('placeholder', 'Raza');
-    inputRaza.setAttribute('autocomplete', 'off');
-    inputRaza.setAttribute('value', this.inputRaza);
-    inputTelefono.setAttribute('type', 'text');
-    inputTelefono.setAttribute('id', 'telefono');
-    inputTelefono.setAttribute('name', 'telefono');
-    inputTelefono.setAttribute('placeholder', 'Telefono');
-    inputTelefono.setAttribute('autocomplete', 'off');
-    inputTelefono.setAttribute('value', this.inputTelefono);
-    inputPais.setAttribute('type', 'text');
-    inputPais.setAttribute('id', 'pais');
-    inputPais.setAttribute('name', 'pais');
-    inputPais.setAttribute('placeholder', 'Pais');
-    inputPais.setAttribute('autocomplete', 'off');
-    inputPais.setAttribute('value', this.inputPais);
-    inputFotografia.setAttribute('type', 'text');
-    inputFotografia.setAttribute('id', 'fotografia');
-    inputFotografia.setAttribute('name', 'fotografia');
-    inputFotografia.setAttribute('placeholder', 'Url de Fotografia');
-    inputFotografia.setAttribute('autocomplete', 'off');
-    inputFotografia.setAttribute('value', this.inputFotografia);
-    textarea.setAttribute('id', 'descripcion');
-    textarea.setAttribute('name', 'descripcion');
-    textarea.setAttribute('placeholder', 'Sobre él/ella');
-    textarea.setAttribute('value', this.inputDescripcion)
-    divButtons.setAttribute('class', 'form__buttons');
-    btnAccept.setAttribute('type', 'submit');
-    btnAccept.setAttribute('class', 'button__accept');
-    btnCancel.setAttribute('type', 'button');
-    btnCancel.setAttribute('class', 'button__cancel');
+  section.setAttribute('class', 'formContainer');
+  form.setAttribute('class', 'form form--overflow');
+  form.setAttribute('action', '#');
+  form.setAttribute('method', 'post');
+  divMensaje.setAttribute('class', 'form__presentation');
+  h2.setAttribute('class', 'form__title');
+  img.setAttribute('class', 'form__iconClose');
+  img.setAttribute('src', './assets/close.svg');
+  img.setAttribute('alt', 'close');
+  divInputs.setAttribute('class', 'form__inputsContainer');
+  inputNombre.setAttribute('type', 'text');
+  inputNombre.setAttribute('id', 'nombre');
+  inputNombre.setAttribute('name', 'nombre');
+  inputNombre.setAttribute('placeholder', 'Nombre');
+  inputNombre.setAttribute('autocomplete', 'off');
+  inputNombre.setAttribute('value', this.inputNombre);
+  inputApellido.setAttribute('type', 'text');
+  inputApellido.setAttribute('id', 'apellido');
+  inputApellido.setAttribute('name', 'apellido');
+  inputApellido.setAttribute('placeholder', 'Apellido');
+  inputApellido.setAttribute('autocomplete', 'off');
+  inputApellido.setAttribute('value', this.inputApellido);
+  inputCorreo.setAttribute('type', 'text');
+  inputCorreo.setAttribute('id', 'correo');
+  inputCorreo.setAttribute('name', 'correo');
+  inputCorreo.setAttribute('placeholder', 'Correo');
+  inputCorreo.setAttribute('autocomplete', 'off');
+  inputCorreo.setAttribute('value', this.inputCorreo);
+  inputTelefono.setAttribute('type', 'text');
+  inputTelefono.setAttribute('id', 'telefono');
+  inputTelefono.setAttribute('name', 'telefono');
+  inputTelefono.setAttribute('placeholder', 'Telefono');
+  inputTelefono.setAttribute('autocomplete', 'off');
+  inputTelefono.setAttribute('value', this.inputTelefono);
+  inputPais.setAttribute('type', 'text');
+  inputPais.setAttribute('id', 'pais');
+  inputPais.setAttribute('name', 'pais');
+  inputPais.setAttribute('placeholder', 'Pais');
+  inputPais.setAttribute('autocomplete', 'off');
+  inputPais.setAttribute('value', this.inputPais);
+  inputFotografia.setAttribute('type', 'text');
+  inputFotografia.setAttribute('id', 'fotografia');
+  inputFotografia.setAttribute('name', 'fotografia');
+  inputFotografia.setAttribute('placeholder', 'Url de Fotografia');
+  inputFotografia.setAttribute('autocomplete', 'off');
+  inputFotografia.setAttribute('value', this.inputFotografia);
+  textarea.setAttribute('id', 'descripcion');
+  textarea.setAttribute('name', 'descripcion');
+  textarea.setAttribute('rows', '8');
+  textarea.setAttribute('placeholder', 'Sobre él/ella');
+  textarea.setAttribute('value', this.inputDescripcion)
+  divButtons.setAttribute('class', 'form__buttons');
+  btnAccept.setAttribute('type', 'submit');
+  btnAccept.setAttribute('class', 'button__accept');
+  btnCancel.setAttribute('type', 'button');
+  btnCancel.setAttribute('class', 'button__cancel');
 
-    h2.textContent = this.mensajeModal;
-    textarea.textContent = this.inputDescripcion
-    btnAccept.textContent = 'Aceptar';
-    btnCancel.textContent = 'Cancelar';
+  h2.textContent = this.mensajeModal;
+  textarea.textContent = this.inputDescripcion;
+  btnAccept.textContent = 'Aceptar';
+  btnCancel.textContent = 'Cancelar';
 
-    divMensaje.appendChild(h2);
-    divMensaje.appendChild(img);
-    divInputs.appendChild(inputNombre);
-    divInputs.appendChild(inputApellido);
-    divInputs.appendChild(inputRaza);
-    divInputs.appendChild(inputTelefono);
-    divInputs.appendChild(inputPais);
-    divInputs.appendChild(inputFotografia);
-    divInputs.appendChild(textarea);
-    divButtons.appendChild(btnAccept);
-    divButtons.appendChild(btnCancel);
-    form.appendChild(divMensaje);
-    form.appendChild(divInputs);
-    form.appendChild(divButtons);
-    section.appendChild(form);
-    body.appendChild(section);
+  divMensaje.appendChild(img);
+  divMensaje.appendChild(h2);
+  divInputs.appendChild(inputNombre);
+  divInputs.appendChild(inputApellido);
+  divInputs.appendChild(inputCorreo);
+  divInputs.appendChild(inputTelefono);
+  divInputs.appendChild(inputPais);
+  divInputs.appendChild(inputFotografia);
+  divInputs.appendChild(textarea);
+  divButtons.appendChild(btnAccept);
+  divButtons.appendChild(btnCancel);
+  form.appendChild(divMensaje);
+  form.appendChild(divInputs);
+  form.appendChild(divButtons);
+  section.appendChild(form);
+  body.appendChild(section);
 
-    img.addEventListener('click', () => {
-        this.cerrarModal();
-    });
+  img.addEventListener('click', () => {
+    this.cerrarModal();
+  });
 
-    btnAccept.addEventListener('click', (event) => {
-        event.preventDefault();
-        this.cerrarModal();
-        app.infoPerro(inputNombre.value, inputApellido.value, inputRaza.value, inputTelefono.value, inputPais.value, inputFotografia.value, textarea.value, index);
-    });
+  btnAccept.addEventListener('click', (event) => {
+    event.preventDefault();
+    this.cerrarModal();
 
-    btnCancel.addEventListener('click', () => {
-        this.cerrarModal();
-    });
+    app.modificarListaPerros(inputNombre.value, inputApellido.value, inputCorreo.value, inputTelefono.value, inputPais.value, inputFotografia.value, textarea.value, index);
+  });
 
-    return this;
+  btnCancel.addEventListener('click', () => {
+    this.cerrarModal();
+  });
+
+  return this;
 };
 
 Modal.prototype.modalEliminar = function (index) {
-    const body = document.getElementById('body');
-    const section = document.createElement('section');
-    const form = document.createElement('form');
-    const divIcon = document.createElement('div');
-    const img = document.createElement('img');
-    const p = document.createElement('p');
-    const divButtons = document.createElement('div');
-    const btnAccept = document.createElement('button');
-    const btnCancel = document.createElement('button');
+  const body = document.getElementById('body');
+  const section = document.createElement('section');
+  const form = document.createElement('form');
+  const divIcon = document.createElement('div');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+  const divButtons = document.createElement('div');
+  const btnAccept = document.createElement('button');
+  const btnCancel = document.createElement('button');
 
-    section.setAttribute('class', 'formContainer');
-    form.setAttribute('class', 'form');
-    form.setAttribute('action', '#');
-    form.setAttribute('method', 'post');
-    img.setAttribute('class', 'form__iconClose');
-    img.setAttribute('src', './assets/close.svg');
-    img.setAttribute('alt', 'close');
-    divButtons.setAttribute('class', 'form__buttons');
-    btnAccept.setAttribute('type', 'submit');
-    btnAccept.setAttribute('class', 'button__accept');
-    btnCancel.setAttribute('type', 'button');
-    btnCancel.setAttribute('class', 'button__cancel');
+  section.setAttribute('class', 'formContainer');
+  form.setAttribute('class', 'form');
+  form.setAttribute('action', '#');
+  form.setAttribute('method', 'post');
+  divIcon.setAttribute('class', 'form__presentation');
+  img.setAttribute('class', 'form__iconClose');
+  img.setAttribute('src', './assets/close.svg');
+  img.setAttribute('alt', 'close');
+  p.setAttribute('class', 'form__questionDelete');
+  divButtons.setAttribute('class', 'form__buttons');
+  btnAccept.setAttribute('type', 'submit');
+  btnAccept.setAttribute('class', 'button__accept');
+  btnCancel.setAttribute('type', 'button');
+  btnCancel.setAttribute('class', 'button__cancel');
 
-    p.textContent = this.mensajeModal;
-    btnAccept.textContent = 'Aceptar';
-    btnCancel.textContent = 'Cancelar';
+  p.textContent = this.mensajeModal;
+  btnAccept.textContent = 'Aceptar';
+  btnCancel.textContent = 'Cancelar';
 
-    divIcon.appendChild(img);
-    divButtons.appendChild(btnAccept);
-    divButtons.appendChild(btnCancel);
-    form.append(divIcon);
-    form.appendChild(p);
-    form.appendChild(divButtons);
-    section.appendChild(form);
-    body.appendChild(section);
+  divIcon.appendChild(img);
+  divButtons.appendChild(btnAccept);
+  divButtons.appendChild(btnCancel);
+  form.append(divIcon);
+  form.appendChild(p);
+  form.appendChild(divButtons);
+  section.appendChild(form);
+  body.appendChild(section);
 
-    img.addEventListener('click', () => {
-        this.cerrarModal();
-    });
+  img.addEventListener('click', () => {
+    this.cerrarModal();
+  });
 
-    btnAccept.addEventListener('click', (event) => {
-        event.preventDefault();
-        this.cerrarModal();
-        app.eliminarInfoPerro(index);
-    });
+  btnAccept.addEventListener('click', (event) => {
+    event.preventDefault();
+    this.cerrarModal();
 
-    btnCancel.addEventListener('click', () => {
-        this.cerrarModal();
-    });
+    app.eliminarCardPerro(index);
+  });
 
-    return this;
+  btnCancel.addEventListener('click', () => {
+    this.cerrarModal();
+  });
+
+  return this;
 };
--- Hackaton13/pquispe/js/classPerro.js
+++ Hackaton13/pquispe/js/classPerro.js
@@ -1,59 +1,59 @@
 
 class Perro {
-    constructor () {
-        this.nombre = '';
-        this.apellido = '';
-        this.raza = '';
-        this.telefono = '';
-        this.pais = '';
-        this.fotografia = '';
-        this.descripcion = '';
-    };
+  constructor() {
+    this.nombre = '';
+    this.apellido = '';
+    this.correo = '';
+    this.telefono = '';
+    this.pais = '';
+    this.fotografia = '';
+    this.descripcion = '';
+  };
 };
 
-Perro.prototype.setNombre = function (nombre) {
-    this.nombre = nombre;
-    return this;
+Perro.prototype.setNombre = function (addNombre) {
+  this.nombre = addNombre;
+  return this;
 };
 
-Perro.prototype.setApellido = function (apellido) {
-    this.apellido = apellido;
-    return this;
+Perro.prototype.setApellido = function (addApellido) {
+  this.apellido = addApellido;
+  return this;
 };
 
-Perro.prototype.setRaza = function (raza) {
-    this.raza = raza;
-    return this;
+Perro.prototype.setCorreo = function (addCorreo) {
+  this.correo = addCorreo;
+  return this;
 };
 
-Perro.prototype.setTelefono = function (telefono) {
-    this.telefono = telefono;
-    return this;
+Perro.prototype.setTelefono = function (addTelefono) {
+  this.telefono = addTelefono;
+  return this;
 };
 
-Perro.prototype.setPais = function (pais) {
-    this.pais = pais;
-    return this;
+Perro.prototype.setPais = function (addPais) {
+  this.pais = addPais;
+  return this;
 };
 
-Perro.prototype.setFotografia = function (fotografia) {
-    this.fotografia = fotografia;
-    return this;
+Perro.prototype.setFotografia = function (addFotografia) {
+  this.fotografia = addFotografia;
+  return this;
 };
 
-Perro.prototype.setDescripcion = function (descripcion) {
-    this.descripcion = descripcion;
-    return this;
+Perro.prototype.setDescripcion = function (addDescripcion) {
+  this.descripcion = addDescripcion;
+  return this;
 };
 
 Perro.prototype.build = function () {
-    return {
-        nombre: this.nombre,
-        apellido: this.apellido,
-        raza: this.raza,
-        telefono: this.telefono,
-        pais: this.pais,
-        fotografia: this.fotografia,
-        descripcion: this.descripcion
-    };
+  return {
+    nombre: this.nombre,
+    apellido: this.apellido,
+    correo: this.correo,
+    telefono: this.telefono,
+    pais: this.pais,
+    fotografia: this.fotografia,
+    descripcion: this.descripcion
+  };
 };
