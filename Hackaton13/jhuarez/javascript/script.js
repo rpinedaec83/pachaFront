f2fb8d8fcdeb929a5a2cb949f69f5aeab1bb8564
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-06-04 21:03:53 -0500

    agregar perro, editar perro completo

--- Hackaton13/jhuarez/javascript/script.js
+++ Hackaton13/jhuarez/javascript/script.js
@@ -9,8 +9,8 @@ class Modal {
       this.acceptBtn = document.getElementById('acceptBtn');
       this.cancelBtn = document.getElementById('cancelBtn');
   
-      this.openBtn1.addEventListener('click', this.openModal.bind(this));
-      this.openBtn2.addEventListener('click', this.openModal.bind(this));
+      this.openBtn1.addEventListener('click', this.agregarNuevoPerro.bind(this));
+      this.openBtn1.addEventListener('click', this.agregarNuevoPerro.bind(this));
       this.acceptBtn.addEventListener('click', this.accept.bind(this));
       this.cancelBtn.addEventListener('click', this.cancel.bind(this));
       window.addEventListener("click", this.closeModal.bind(this));
@@ -26,21 +26,119 @@ class Modal {
             this.modal.style.display = 'none';
         }
     };
+
+    agregarNuevoPerro() {
+      // Restablecer los valores del formulario
+      const formulario = document.getElementById("formularioPerro");
+      formulario.reset();
+      
+      // Abrir el modal
+      this.openModal();
+    }
   
     accept() {
-      console.log('Botón Aceptar presionado');
-      this.modal.style.display = 'none';
+      const index = this.acceptBtn.dataset.index; // Obtén el índice del perro
+      const perros = JSON.parse(localStorage.getItem("perros")); // Obtén los perros del almacenamiento local
+      const perro = perros[index]; // Obtén el perro correspondiente al índice
+    
+      // Actualiza los valores del perro con los valores del formulario
+      perro.nombre = document.getElementById("nombrePerro").value;
+      perro.apellido = document.getElementById("apellidoPerro").value;
+      perro.raza = document.getElementById("razaPerro").value;
+      perro.telefono = document.getElementById("telefonoPerro").value;
+      perro.pais = document.getElementById("paisPerro").value;
+      perro.descripcion = document.getElementById("descripcionPerro").value;
+    
+      // Guarda los perros actualizados en el almacenamiento local
+      localStorage.setItem("perros", JSON.stringify(perros));
+    
+      // Cierra el modal y actualiza la lista de perros en la página
+      this.modal.style.display = "none";
+      mostrarPerrosEnPagina();
     }
   
     cancel() {
       console.log('Botón Cancelar presionado');
       this.modal.style.display = 'none';
     }
+
+    llenarFormulario(perro) {
+      const nombrePerro = document.getElementById("nombrePerro");
+      const apellidoPerro = document.getElementById("apellidoPerro");
+      const razaPerro = document.getElementById("razaPerro");
+      const telefonoPerro = document.getElementById("telefonoPerro");
+      const paisPerro = document.getElementById("paisPerro");
+      const descripcionPerro = document.getElementById("descripcionPerro");
+  
+      nombrePerro.value = perro.nombre;
+      apellidoPerro.value = perro.apellido;
+      razaPerro.value = perro.raza;
+      telefonoPerro.value = perro.telefono;
+      paisPerro.value = perro.pais;
+      descripcionPerro.value = perro.descripcion;
+    }
+
+    aceptarEdicion(event, index, perros) {
+      event.preventDefault();
+    
+      // Obtener los valores actualizados del formulario
+      const nombrePerro = document.getElementById("nombrePerro").value;
+      const apellidoPerro = document.getElementById("apellidoPerro").value;
+      const razaPerro = document.getElementById("razaPerro").value;
+      const telefonoPerro = document.getElementById("telefonoPerro").value;
+      const paisPerro = document.getElementById("paisPerro").value;
+      const descripcionPerro = document.getElementById("descripcionPerro").value;
+    
+      // Obtener el campo de entrada de archivo
+      const inputFoto = document.getElementById("fotoPerro");
+    
+      // Obtener el archivo de imagen seleccionado
+      const nuevaFotoPerro = inputFoto.files[0];
+    
+      // Actualizar los valores del perro seleccionado
+      const perro = perros[index];
+      perro.nombre = nombrePerro;
+      perro.apellido = apellidoPerro;
+      perro.raza = razaPerro;
+      perro.telefono = telefonoPerro;
+      perro.pais = paisPerro;
+      perro.descripcion = descripcionPerro;
+    
+      // Actualizar la imagen del perro si se seleccionó una nueva imagen
+      if (nuevaFotoPerro) {
+        const reader = new FileReader();
+    
+        reader.onload = function (event) {
+          const foto = event.target.result;
+          perro.foto = foto;
+    
+          // Actualizar el array de perros en el almacenamiento local
+          localStorage.setItem("perros", JSON.stringify(perros));
+    
+          // Cerrar el modal y actualizar la lista de perros en la página
+          modal.modal.style.display = "none";
+          mostrarPerrosEnPagina();
+          resetFileInput(); // Restablecer el campo de entrada de archivo
+        };
+    
+        reader.readAsDataURL(nuevaFotoPerro);
+      } else {
+        // No se seleccionó una nueva imagen, solo actualizar los valores existentes
+    
+        // Actualizar el array de perros en el almacenamiento local
+        localStorage.setItem("perros", JSON.stringify(perros));
+    
+        // Cerrar el modal y actualizar la lista de perros en la página
+        modal.modal.style.display = "none";
+        mostrarPerrosEnPagina();
+        resetFileInput(); // Restablecer el campo de entrada de archivo
+      }
+    }
 }
 
 
 class Perro {
-  constructor(nombre, apellido, raza, telefono, pais, foto, descripcion) {
+  constructor(nombre, apellido, raza, telefono, pais, foto, descripcion, index) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.raza = raza;
@@ -48,12 +146,14 @@ class Perro {
     this.pais = pais;
     this.foto = foto;
     this.descripcion = descripcion;
+    this.index = index;
   }
+
 }
 
 const modal = new Modal();
 
-let perros=[];
+//let perros=[];
 
 document.getElementById("formularioPerro").addEventListener("submit", async function(event) {
   
@@ -66,6 +166,8 @@ document.getElementById("formularioPerro").addEventListener("submit", async func
   const inputFoto = document.getElementById("fotoPerro").files[0]; // Obtén el archivo de imagen seleccionado
   const descripcion = document.getElementById("descripcionPerro").value;
 
+  perros = await obtenerPerros();
+
   if (event.submitter.id === "acceptBtn") {
     if (nombre && apellido && raza && telefono && pais && inputFoto && descripcion) {
 
@@ -74,7 +176,7 @@ document.getElementById("formularioPerro").addEventListener("submit", async func
       reader.onload = function(event) {
 
         const foto = event.target.result; // Obtén la representación base64 de la imagen
-        const nuevoPerro = new Perro(nombre, apellido, raza, telefono, pais, foto, descripcion);
+        const nuevoPerro = new Perro(nombre, apellido, raza, telefono, pais, foto, descripcion, perros.length); // Agregamos perros.length como el índice del nuevo perro
 
         perros.push(nuevoPerro);
 
@@ -124,20 +226,39 @@ async function mostrarPerrosEnPagina() {
     botonAgregarMascota1.classList.toggle("oculto", perros.length > 0);
     botonAgregarMascota2.classList.toggle("oculto", perros.length === 0);
 
-    perros.forEach((perro) => {
+    perros.forEach((perro, index) => {
       const perroHTML = `
-        <div>
+        <li>
+          <img src="${perro.foto}" alt="Foto del perro">
           <h3>${perro.nombre} ${perro.apellido}</h3>
           <p>Raza: ${perro.raza}</p>
           <p>Teléfono: ${perro.telefono}</p>
-          <p>País: ${perro.pais}</p>
-          <img src="${perro.foto}" alt="Foto del perro">
+          <p>País: ${perro.pais}</p>          
           <p>Descripción: ${perro.descripcion}</p>
-        </div>
+          <button class="btn-editar" data-index="${index}">Editar</button>
+          <button class="btn-eliminar" data-index="${index}">Eliminar</button>
+        </li>
       `;
 
       contenedorPerros.innerHTML += perroHTML;
     });
+
+    // Agregar eventos a los botones de editar y eliminar
+    const botonesEditar = document.getElementsByClassName("btn-editar");
+    const botonesEliminar = document.getElementsByClassName("btn-eliminar");
+
+    for (let i = 0; i < botonesEditar.length; i++) {
+      botonesEditar[i].addEventListener("click", function (event) {
+        editarPerro(event, perros);
+      });
+    }
+
+    for (let i = 0; i < botonesEliminar.length; i++) {
+      botonesEliminar[i].addEventListener("click", function (event) {
+        eliminarPerro(event, perros);
+      });
+    }
+
   } catch (error) {
     console.log(error);
   }
@@ -153,3 +274,21 @@ function obtenerPerros() {
     }
   });
 }
+
+function editarPerro(event, perros) {
+  const index = event.target.dataset.index;
+  const perro = perros[index];
+  modal.llenarFormulario(perro);
+  modal.openModal();
+  modal.acceptBtn.dataset.index = index;
+  modal.acceptBtn.removeEventListener("click", modal.accept);
+  modal.acceptBtn.addEventListener("click", function (event) {
+    modal.aceptarEdicion(event, index, perros); // Cambiar evento "submit" a "click"
+  });
+}
+
+function resetFileInput() {
+  const inputFoto = document.getElementById("fotoPerro");
+  inputFoto.value = ""; // Restablecer el valor del campo de entrada de archivo
+}
+
