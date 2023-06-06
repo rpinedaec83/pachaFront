754a0f4da953dc4a092809da60cdc0c18a4e034c
Roberto Pineda <rpineda@MacBook-Pro-de-Roberto.local>
2023-06-05 22:36:56 -0500

    Clase 05062023 Parte 2

--- Semana14Sesion01/rpineda/parte1/app.js
+++ Semana14Sesion01/rpineda/parte1/app.js
@@ -1,5 +1,6 @@
 const app = function () {
     let urOMDb = "https://www.omdbapi.com/";
+    let urlGitFake = ""
     const apikey = "1905e990";
     function sl() {
         $('.modal').modal('show');
@@ -33,9 +34,25 @@ function dibujarPelicula(data){
         title: `<strong>${data.Title}</strong>`,
         icon: 'info',
         html:
-          'You can use <b>bold text</b>, ' +
-          '<a href="//sweetalert2.github.io">links</a> ' +
-          'and other HTML tags',
+          `<div class="card rounded">
+          <div class="card-image">
+              <span class="card-notify-badge">${data.Genre}</span>
+              <span class="card-notify-year">${data.Year}</span>
+              <img class="img-fluid" src="${data.Poster}" alt="Alternate Text" />
+          </div>
+          <div class="card-image-overlay m-auto">
+              <span class="card-detail-badge">Recaudacion:</span>
+              <span class="card-detail-badge">${data.BoxOffice}</span>
+              <span class="card-detail-badge">Rating: </span>
+              <span class="card-detail-badge">${data.imdbRating}</span>
+          </div>
+          <div class="card-body text-center">
+              <div class="ad-title m-auto">
+                  <h5>Honda Accord LX</h5>
+              </div>
+              <a class="ad-btn" href="#">View</a>
+          </div>
+      </div>`,
         showCloseButton: true,
         showCancelButton: true,
         focusConfirm: false,
@@ -48,12 +65,52 @@ function dibujarPelicula(data){
       })
 }
     let cargarInformacionInicial = function () {
-        fetch(`https://www.omdbapi.com/?apikey=${apikey}&s=Mave`).then(
-            response => response.json()
-        ).then(
-            json=>console.log(json)
-        );
+        cargarProfile()
     }
+   function cargarProfile(){
+
+    fetch(`${urlGitFake}profile`).then(
+        response => response.json()
+    ).then(
+        json=>dibujarPerfil(json)
+        
+    );
+   }
+  function dibujarPerfil(data){
+    Swal.fire({
+        title: `<strong>${data.login}</strong>`,
+        icon: 'info',
+        html:
+          `<div class="card rounded">
+          <div class="card-image">
+              <span class="card-notify-badge">${data.followers}</span>
+              <span class="card-notify-year">${data.public_repos}</span>
+              <img class="img-fluid" src="${data.avatar_url}" alt="Alternate Text" />
+          </div>
+          <div class="card-image-overlay m-auto">
+              <span class="card-detail-badge">Tipo:</span>
+              <span class="card-detail-badge">${data.type}</span>
+              <span class="card-detail-badge">Id: </span>
+              <span class="card-detail-badge">${data.id}</span>
+          </div>
+          <div class="card-body text-center">
+              <div class="ad-title m-auto">
+                  <h5>Honda Accord LX</h5>
+              </div>
+              <a class="ad-btn" href="#">View</a>
+          </div>
+      </div>`,
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> Great!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+        cancelButtonText:
+          '<i class="fa fa-thumbs-down"></i>',
+        cancelButtonAriaLabel: 'Thumbs down'
+      })
+  }
     function cargarTabla(data){
         var $table = $('#table')
         $table.bootstrapTable('load',data)
@@ -71,6 +128,7 @@ function dibujarPelicula(data){
     return {
         init: function (parametros=null) {
             urOMDb = parametros.urOMDb
+            urlGitFake = parametros.urlGitFake
             eventos();
             cargarInformacionInicial();
         }
