a203db796e8eb5901ef646287ce8bf79e971620b
Heri Roque <heriroque.22@gmail.com>
2023-06-17 10:40:01 -0500

    Hackaton15 desarrollo

---
+++ Hackaton15/hroque/Reto03 - Pachatube/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <nav class="flex justify-between p-4 bg-[#FF5252] h-16 items-center">
+    <RouterLink to="/">
+      <h1 class="text-lg md:text-xl lg:text-3xl font-bold text-white uppercase">Video Pacha Tv</h1>
+    </RouterLink>
+    <RouterLink v-show="mostrar" to="/crear" class="px-5 py-2 text-white font-bold">Crear Video</RouterLink>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+      mostrar: {
+          type: Boolean,
+          required: true
+      },
+  },
+}
+</script>
+<style lang="">
+    
+</style>
