504956dec31f0df717f30c4a0a2a2971bec3d933
Manuel <frama9125@gmail.com>
2023-06-17 10:23:51 -0500

    Se envian retos de la hackaton 15

---
+++ Hackaton15/mfrancia/reto02-03/src/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">VIDEO TV</h1>
+    <slot name="header-options"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #FF5252;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.26);
+  z-index: 1;
+}
+
+.header__title {
+  color: #fff;
+  font-size: 3.6rem;
+  font-style: normal;
+  font-weight: 700;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ Hackaton15/mfrancia/reto02-03/src/components/VideoDetails.vue
@@ -0,0 +1,85 @@
+<template>
+  <main class="main">
+    <h2 class="main__title">{{ detailsVideo[0] }}</h2>
+    <div class="itemCard__videoContainer">
+      <iframe :src="modifiedUrl(detailsVideo[1])" :title="detailsVideo[0]" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    </div>
+    <h3 class="main__title2">{{ detailsVideo[0] }}</h3>
+    <span class="main__views">{{ viewsFormat(detailsVideo[3]) }} visualizaciones</span>
+    <p class="main__description">{{ detailsVideo[2] }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  props: {
+    detailsVideo: Array
+  },
+  computed: {
+    modifiedUrl() {
+      return (url) => {
+        return url.replace('/watch?v=', '/embed/');
+      };
+    },
+    viewsFormat() {
+      return (views) => {
+        return views.toLocaleString();
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  padding: 60px 30px 100px;
+}
+
+.main__title {
+  color: #000;
+  font-size: 2.8rem;
+  font-style: normal;
+  font-weight: 700;
+  margin-bottom: 60px;
+}
+
+.itemCard__videoContainer {
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  margin-bottom: 40px;
+}
+
+.itemCard__videoContainer iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.main__title2 {
+  color: #000;
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+
+.main__views {
+  color: #303030;
+  font-size: 1.1rem;
+  font-style: normal;
+  font-weight: 400;
+}
+
+.main__description {
+  color: #808080;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 400;
+  margin-top: 15px;
+}
+</style>
